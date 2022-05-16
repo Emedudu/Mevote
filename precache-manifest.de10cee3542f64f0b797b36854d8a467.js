ffcf58ffb7524b9390a2b55b46ab5c3b60ff32c1
meduduna Akerejola <cremmybanj7@gmail.com>
2022-05-16 14:02:26 +0100

    Updates

---
+++ precache-manifest.de10cee3542f64f0b797b36854d8a467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ac9f4aa15e0a5f52a72",
+    "url": "/Mevote_Frontend/static/css/main.9359e1f9.chunk.css"
+  },
+  {
+    "revision": "5ac9f4aa15e0a5f52a72",
+    "url": "/Mevote_Frontend/static/js/main.5ac9f4aa.chunk.js"
+  },
+  {
+    "revision": "372adfe5196b01b518b0",
+    "url": "/Mevote_Frontend/static/css/1.ea6e69fe.chunk.css"
+  },
+  {
+    "revision": "372adfe5196b01b518b0",
+    "url": "/Mevote_Frontend/static/js/1.372adfe5.chunk.js"
+  },
+  {
+    "revision": "442fc5c907af750ccfa7",
+    "url": "/Mevote_Frontend/static/js/runtime~main.442fc5c9.js"
+  },
+  {
+    "revision": "95fb22cc67376cd21840c3f11e772f77",
+    "url": "/Mevote_Frontend/index.html"
+  }
+];
